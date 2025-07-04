3826ebe53164cf4f790584869294e7b555cba0bb
misson-infinity <shortstoriesbgc25@gmail.com>
2025-07-04 17:41:17 +0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,10 @@
-5
+import React from 'react';
+const Footer = () => {
+    const year = new Date().getFullYear();
+    return (
+        <footer className="footer">
+            <p>© {year} I Expense Tracker | Developed by Md Habibur Rahman Mahi, founder and CEO of infinity group ♾️</p>
+        </footer>
+    );
+};
+export default Footer;
